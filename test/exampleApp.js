174f7787b3b10f91f78cf50bf24e593916623596
Sean Clark <imclarky@users.noreply.github.com>
2017-06-15 11:47:21 +0100

    initial commit

---
+++ test/exampleApp.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var Crumb = require('../');
+var http = require('http');
+var app = express();
+
+var crumb = new Crumb()
+
+app.get("/", function(req, res, next) {
+  res.send("home")
+});
+
+app.get("/test", function(req, res, next) {
+  res.send("test")
+});
+
+app.get("/test/foo/bar", crumb.generateCrumbs({includeHome:false,includeNonRoutes:true, linkLastCrumb:true}), function(req, res, next){
+  res.send(res.locals.crumbs)
+})
+
+crumb.getRoutes(app)
+
+var server = http.createServer(app)
+server.listen(3000)
